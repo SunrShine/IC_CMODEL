4d5f0e457bad0f2baabce4bbb526e4cef021d4a8
SunrShine <48122125+SunrShine@users.noreply.github.com>
2024-05-26 19:34:27 +0800

    v0.01

--- lib/BaseLine/PipRegDelay.h
+++ lib/BaseLine/PipRegDelay.h
@@ -5,13 +5,12 @@
 #include <cstdint>
 #include <vector>
 
-template <class T>
-class PipRegDelay {
+template <class T> class PipRegDelay {
 
 public:
   PipRegDelay() = default;
   ~PipRegDelay(){};
-  PipRegDelay(PipRegDelay &pip)                  = delete;
+  PipRegDelay(PipRegDelay &pip) = delete;
   PipRegDelay &operator=(const PipRegDelay &pip) = delete;
 
   void init();
@@ -23,26 +22,27 @@ private:
    * @brief delay Queue and Output Reg
    *
    */
-  size_t _delay_cycle;
+  size_t _delay_cycle = 1;
   std::vector<T *> _delayQ;
-  T               *outQpVal;
-  size_t           _atQin;
-  size_t           _atQout;
-  size_t           _atQin_next;
-  size_t           _atQout_next;
+  T *_outQVal = nullptr;
+  T *_inQVal = nullptr;
+  size_t _atQin = 0;
+  size_t _atQout = 1;
+  size_t _atQin_next = 1;
+  size_t _atQout_next = 2;
 };
 
-template <class T>
-void PipRegDelay<T>::compulte(T *input) {
-  _delayQ.at(_atQin);
-  _atQin_next = () ?;
+template <class T> void PipRegDelay<T>::compulte(T *input) {
+  _inQVal = input;
+  _atQin_next = (_atQin + 1) % _delay_cycle;
+  _atQout_next = (_atQin_next + 1) % _delay_cycle;
 }
 
-template <class T>
-void PipRegDelay<T>::update() {
+template <class T> void PipRegDelay<T>::update() {
+  _delayQ.at(_atQin) = _inQVal;
+  _outQVal = _delayQ.at(_atQin);
   _atQin = _atQin_next;
   _atQout = _atQout_next;
-  
 }
 
 #endif
